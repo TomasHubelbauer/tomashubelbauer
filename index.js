5e87ae64035ab376668f076f0fa35d4e9efdfe54
Tomas Hubelbauer <tomas@hubelbauer.net>
2021-03-27 19:48:30 +0100

    Display only the first line of the commit message

--- index.js
+++ index.js
@@ -373,11 +373,11 @@ function name(name) {
 function commit(repo, payload) {
   if (payload.commits.length === 1) {
     const commit = payload.commits[0];
-    return `\n  [${commit.message}](https://github.com/${repo.name}/commit/${commit.sha})\n  into${name(repo.name)}`;
+    return `\n  [${commit.message.match(/^.*/g)[0]}](https://github.com/${repo.name}/commit/${commit.sha})\n  into${name(repo.name)}`;
   }
 
   const commit = payload.commits[payload.commits.length - 1];
-  return `\n  [${commit.message}](https://github.com/${repo.name}/commit/${commit.sha})\n  and ${payload.commits.length - 1} other${payload.commits.length - 2 ? 's' : ''} into${name(repo.name)}`;
+  return `\n  [${commit.message.match(/^.*/g)[0]}](https://github.com/${repo.name}/commit/${commit.sha})\n  and ${payload.commits.length - 1} other${payload.commits.length - 2 ? 's' : ''} into${name(repo.name)}`;
 }
 
 function branch(repo, payload) {
