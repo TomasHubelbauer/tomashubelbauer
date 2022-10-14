bdde28d47c761159aee9c582334f7cb4afeb19a7
Tomas Hubelbauer <tomas@hubelbauer.net>
2022-10-14 21:28:27 +0200

    Pull date from the event not the surrounding context
    This was an ommission where one of the writers wasn't a pure function of the event.

--- writePushEvent.js
+++ writePushEvent.js
@@ -5,7 +5,7 @@ import time from './time.js';
 export default function writePushEvent(event) {
   let markdown = `📌 pushed${commit(event.repo, event.payload)}`;
   for (let index = 1; index < event.payload.commits.length; index++) {
-    markdown += `\n- \`${time(_date)}\`\n  `;
+    markdown += `\n- \`${time(new Date(event.created_at))}\`\n  `;
     markdown += `📌 pushed${commit(event.repo, event.payload, index)}`;
   }
 
