b81c6e1d4cf2750c0dafa438538d54d493707d00
Tomas Hubelbauer <tomas@hubelbauer.net>
2025-12-04 19:10:18 +0100

    Log commitless payload of a push event

--- commit.js
+++ commit.js
@@ -1,6 +1,10 @@
 import name from './name.js';
 
 export default function commit(repo, payload, index = 0) {
+  if (!payload.commits) {
+    console.log({ repo, payload });
+  }
+
   const commit = [...payload.commits].reverse()[index];
 
   return `\n  [${commit.message.match(/^.*/g)[0]}](https://github.com/${repo.name}/commit/${commit.sha})\n  into${name(repo.name)}`;
