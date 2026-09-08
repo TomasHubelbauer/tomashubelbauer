6cb6f12b04e0e1c92e99dc55d1b2171db4055f43
anupamme <mediratta@gmail.com>
2026-09-08 19:39:20 +0000

    fix: V-001 security vulnerability
    Automated security fix generated by OrbisAI Security

--- headers.ts
+++ headers.ts
@@ -1,3 +1,3 @@
 import login from './login.ts';
 
-export default { 'User-Agent': login, Authorization: process.argv[2] ? 'token ' + process.argv[2] : '' };
+export default { 'User-Agent': login, Authorization: process.env.GITHUB_TOKEN ? 'token ' + process.env.GITHUB_TOKEN : '' };
